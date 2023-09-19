257bb5513e205d6e9d4050a69a8e8f6d03cd5832
Niel <palaciosp.daniel@outlook.com>
2023-09-19 13:25:19 +0200

    playerMovement

---
+++ Scripts/Player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export_subgroup("Components")
+@export var sprite: Sprite2D
+
+@export_subgroup("Properties")
+@export var movement_speed: float = 400.0
+
+var movement_velocity: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	handle_controls(delta)
+	handle_position(delta)
+	
+	move_and_slide()
+
+func handle_controls(delta):
+	# Movement
+	var input := Vector2.ZERO
+	
+	input.x = Input.get_axis("move_left", "move_right")
+	input.y = Input.get_axis("move_up", "move_down")
+	
+	velocity = input * movement_speed
+	
+func handle_position(delta):
+	position += velocity * delta
+	
+	if velocity.length() == 0: return
+	
+	sprite.flip_h = velocity.x < 0
+
