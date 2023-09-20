373f812575d24aabb0929256606f8aa6abaef52c
Niel <palaciosp.daniel@outlook.com>
2023-09-20 16:27:50 +0200

    complete events con interact

--- Scripts/event.gd
+++ Scripts/event.gd
@@ -8,10 +8,12 @@ extends Node2D
 
 var try_count := 0.0;
 var isActive := false;
+var isPlayer := false;
 
 # Components
 @onready var notification = $notification
 @onready var trigger = $hint_trigger
+@onready var hint = $hint
 @onready var rng = RandomNumberGenerator.new()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,13 +23,29 @@ func _process(delta):
 		start_event()
 		try_count = 0
 
+# Wait for player to use interact in order to complete event
+func _input(event):
+	if event.is_action_released("Interact") && isActive && isPlayer:
+		complete_event()
+
 # Try's to init the event using the given %
 func start_event():
-	print_debug("Trying to start Event")
 	if rng.randf_range(0.0, 1.0) > spawn_probability / 100:
-		print_debug("Event initiated")
 		isActive = true
 		show()
-		
 
+# Disable the event and wait x seconds before beeing available for activation
+func complete_event():
+	hide()
+	await get_tree().create_timer(delay).timeout
+	isActive = false
+	try_count = 0
 
+# Player detection
+func _on_hint_trigger_body_entered(body):
+	hint.show()
+	isPlayer = true
+
+func _on_hint_trigger_body_exited(body):
+	hint.hide()
+	isPlayer = false
